68c108a27c361f3f810d8ad9b611156642f1734f
Drew Tayman <dretay@gmail.com>
2019-07-15 00:55:44 -0400

    logging cleanup

--- log.h
+++ log.h
@@ -1,28 +1,26 @@
 #pragma once
 
 //simple debug logging
-#include "SEGGER_RTT.h"
 #include "stm32f1xx_hal.h"
 #include <string.h>
-
-
+#include <stdarg.h>
 
 void printfUART(const char *fmt, ...);
 
 #define LOG(...) printfUART(__VA_ARGS__)
 
-#define _ERROR(fmt, args...) LOG("\r\n%s%s[ERROR] %s%s:%s:%d: "fmt, RTT_CTRL_RESET, RTT_CTRL_BG_BRIGHT_RED, RTT_CTRL_RESET, __FILE__, __FUNCTION__, __LINE__, args)
+#define _ERROR(fmt, args...) LOG("\r\n[ERROR] %s%s:%s:%d: "fmt, __FILE__, __FUNCTION__, __LINE__, args)
 #define __ERROR(...) printfUART(__VA_ARGS__)
 
-#define _WARN(fmt, args...) LOG("\r\n%s%s[WARN] %s%s:%s:%d: "fmt, RTT_CTRL_RESET, RTT_CTRL_BG_BRIGHT_YELLOW, RTT_CTRL_RESET, __FILE__, __FUNCTION__, __LINE__, args)
+#define _WARN(fmt, args...) LOG("\r\n[WARN] %s%s:%s:%d: "fmt, __FILE__, __FUNCTION__, __LINE__, args)
 #define __WARN(...) printfUART(__VA_ARGS__)
 
 
-#define _INFO(fmt, args...) LOG("\r\n%s[INFO] %s:%s:%d: "fmt, RTT_CTRL_RESET, __FILE__, __FUNCTION__, __LINE__, args)
+#define _INFO(fmt, args...) LOG("\r\ns[INFO] %s:%s:%d: "fmt, __FILE__, __FUNCTION__, __LINE__, args)
 #define __INFO(...) printfUART(__VA_ARGS__)
 
 
-#define _DEBUG(fmt, args...) LOG("\r\n%s[DEBUG] %s:%s:%d: "fmt, RTT_CTRL_RESET, __FILE__, __FUNCTION__, __LINE__, args)
+#define _DEBUG(fmt, args...) LOG("\r\n[DEBUG] %s:%s:%d: "fmt, __FILE__, __FUNCTION__, __LINE__, args)
 #define __DEBUG(...) printfUART(__VA_ARGS__)
 
 
